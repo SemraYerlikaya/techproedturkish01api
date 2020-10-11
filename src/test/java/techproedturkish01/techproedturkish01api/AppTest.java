77b984cb04d507c34e79a0224ee905c6070578b8
apple <apple@apples-imac-6.attlocal.net>
2020-10-11 02:07:10 -0400

    methods

--- src/test/java/techproedturkish01/techproedturkish01api/AppTest.java
+++ src/test/java/techproedturkish01/techproedturkish01api/AppTest.java
@@ -1,20 +1,58 @@
 package techproedturkish01.techproedturkish01api;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertTrue;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class AppTest{
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+    
+    @Test
+	public void get01() {
+		given().
+		when().
+		   get("https://restful-booker.herokuapp.com/booking").
+		then().
+		   assertThat().
+		   statusCode(200);		
+	}
+    
+    @Test
+	public void get02() {
+		Response response = given().
+		                       accept("application/json").
+		                    when().
+		                       get("https://restful-booker.herokuapp.com/booking/1001");	
+		response.prettyPrint();		
+		response.
+		then().
+		assertThat().
+		statusCode(404);
+	}
+    @Test
+	public void get03() {
+		Response response = given().
+				               accept(ContentType.JSON).
+				            when().
+				               get("http://dummy.restapiexample.com/api/v1/employees"); 
+		response.prettyPrint();	
+		response.
+		   then().
+		   assertThat().
+		   statusCode(200).
+		   contentType(ContentType.JSON).
+		   body("data.id", Matchers.hasSize(24)).
+		   body("data.employee_name", Matchers.hasItem("Ashton Cox")).
+		   body("data.employee_age",Matchers.hasItems("21", "61", "23"));
+	}
 
 }
